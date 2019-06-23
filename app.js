fc29a7f7af415e5b273863750d682a0f328692c2
Scott Willis <scott.willis.wsu@gmail.com>
2019-06-22 22:21:43 -0700

    need to finish work history

---
+++ app.js
@@ -0,0 +1,44 @@
+$("#portfolio-btn").on("click", function () {
+  window.location.href = "portfolio.html"
+})
+
+
+$("#deployQuiz").on("click", function () {
+  window.open("https://willissa2121.github.io/homework5/")
+})
+
+$("#githubQuiz").on("click", function () {
+  window.open("https://github.com/willissa2121/homework5")
+})
+
+$("#deployCarpool").on("click", function () {
+  window.open("https://andrewjoung.github.io/carpoolify/")
+})
+
+$("#githubCarpool").on("click", function () {
+  window.open("https://github.com/andrewjoung/carpoolify")
+})
+
+$("#deployStarwars").on("click", function () {
+  window.open("https://willissa2121.github.io/homework4/")
+})
+
+$("#githubStarwars").on("click", function () {
+  window.open("https://github.com/willissa2121/homework4")
+})
+
+$("#deployHangman").on("click", function () {
+  window.open("https://willissa2121.github.io/homework3-2/")
+})
+
+$("#githubHangman").on("click", function () {
+  window.open("https://github.com/willissa2121/homework3-2")
+})
+
+$("#deployGif").on("click", function () {
+  window.open("https://willissa2121.github.io/homework6/")
+})
+
+$("#githubGif").on("click", function () {
+  window.open("https://github.com/willissa2121/homework6")
+})
